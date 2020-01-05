66eaa0e7b9d075580c42c5b040961290ed576c09
cScarlson <otocarlson@gmail.com>
2020-01-04 17:03:01 -0700

    Build: testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 
 // https://webpack.js.org/configuration/dev-server/
+require('dotenv').config();
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyPlugin = require('copy-webpack-plugin');
 var { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//
+var config = require('./app.json');
+
+var { env: __env__ } = process
+  , { NODE_ENV: __type__ } = __env__
+  ;
+console.log('?\n\n', __type__);
+var { environments: env } = config;
+var { production, development } = env;
+var OPTIONS = env[ __type__ ];
+var BASE_HREF = OPTIONS.baseUrl
+  , X
+  ;
 
 module.exports = {
     entry: path.resolve(__dirname, './src/main.ts'),
@@ -25,7 +39,7 @@ module.exports = {
         title: 'Cody S. Carlson',
         filename: 'index.html',
         template: './src/index.html',
-        base: '/dist',
+        baseUrl: BASE_HREF,
         favicon: './src/assets/shadow.jpg',
       }),
       new CopyPlugin([

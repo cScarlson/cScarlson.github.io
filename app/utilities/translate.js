2079251ab9099469c1ded0058f8a4ad507ddc09d
cScarlson <otocarlson@gmail.com>
2022-09-21 15:14:38 -0600

    -1: deployment 2...

--- app/utilities/translate.js
+++ app/utilities/translate.js
@@ -1,5 +1,5 @@
 
-import { Translator } from '/app/core/l10n/Translator.js';
+import { Translator } from '/app/core/l10n/translator.js';
 import { default as en } from '/app/locales/en.locale.js';
 
 const locales = { en, es: en };

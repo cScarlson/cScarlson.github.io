d96a7c8b2d08c4e7dc084e47243aa48bdff94203
cScarlson <otocarlson@gmail.com>
2026-02-18 01:48:57 -0700

    -1: cleanup...

--- asxs/src/lib/core/frameless/frameless.element.ts
+++ asxs/src/lib/core/frameless/frameless.element.ts
@@ -14,13 +14,11 @@ const { log } = console;
 export const TAGNAME = 'as-frameless';
 export @customElement(TAGNAME, { extends: 'iframe' }) class FramelessElement extends HTMLIFrameElement {
     #deferred: ToDo = (Promise as ToDo).withResolvers();
-    #lightdom: HTMLCollection = document.createElement('div').children;
-    #parent: any = null;
     
     constructor() {
         super();
-        this.width = '0';
-        this.height = '0';
+        this.width = '0';  // ensure no iframe actually appears to user
+        this.height = '0';  // ensure no iframe actually appears to user
     }
     
     #initialize = () => {
@@ -53,23 +51,22 @@ export @customElement(TAGNAME, { extends: 'iframe' }) class FramelessElement ext
             const { shadowRoot } = element as HTMLUnknownElement;
             const shadow = shadowRoot!;
             
+            for (const child of slotted) element.appendChild(child);  // strongly-slotted content
             shadow.appendChild(styles);
             shadow.appendChild(content);
-            for (const child of slotted) element.appendChild(child);
             element.removeEventListener('load', handleScriptLoad, true);
             module.remove();
             sentinel.remove();
         }
         
-        log(`@compose... %O`, this, innerHTML, element, this.childNodes);
         module.innerHTML = script.innerHTML;
-        element.innerHTML = innerHTML;
+        element.innerHTML = innerHTML;  // weakly-slotted content
         element.addEventListener('load', handleScriptLoad, true);
         element.appendChild(module);
         element.appendChild(sentinel);
         if (!parentElement) this.replaceWith(element);
         else if (parentElement.matches('as-frameless-slots')) parentElement.replaceWith(element);
-        else this.replaceWith(element);
+        else this.replaceWith(element);  // if it has a parent but is not a known wrapper
     }
     
     #handleLoad(e: Event) {

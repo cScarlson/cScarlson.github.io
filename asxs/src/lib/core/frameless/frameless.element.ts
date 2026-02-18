fcbb8912607b4dd3aeb22fb5837837e3a7f05c6c
cScarlson <otocarlson@gmail.com>
2026-02-18 15:31:39 -0700

    -1: CHECKPOINT; as-frameless now delegates all ops (beside slotting) to RED module; REDs must define elements in window.parent

--- asxs/src/lib/core/frameless/frameless.element.ts
+++ asxs/src/lib/core/frameless/frameless.element.ts
@@ -15,6 +15,7 @@ export @customElement(TAGNAME, { extends: 'iframe' }) class FramelessElement ext
         super();
         this.width = '0';  // ensure no iframe actually appears to user
         this.height = '0';  // ensure no iframe actually appears to user
+        this.style.border = 'none';  // ensure no iframe actually appears to user
     }
     
     #initialize = () => {
@@ -25,30 +26,17 @@ export @customElement(TAGNAME, { extends: 'iframe' }) class FramelessElement ext
         this.#compose(children as RemoteElementDefinition);
     };
     
-    #define(script: HTMLScriptElement) {
-        const module = document.createElement('script');
-        
-        module.innerHTML = script.innerHTML;
-        document.body.appendChild(module);
-        
-        return module;
-    }
-    
     #compose(red: RemoteElementDefinition) {
         const { innerHTML: content, parentElement } = this;
         const { [0]: meta, [1]: template, [2]: styles, [3]: script } = red;  // RED (Remote Element Definition) protocol
         const { attributes: { [0]: { value: tagName } } } = meta as HTMLMetaElement;
-        const { innerHTML } = template as HTMLTemplateElement;
-        const nodes = getSlottedNodes(parentElement!);
+        const nodes = getSlottedNodes(parentElement);
         const slotted = nodes.filter(child => child !== this);
-        const module = this.#define(script);
         const Class = customElements.get(tagName) as typeof Frameless;
         const element = new Class({ meta, template, styles, script });  // should everything after this be done by the RED?
-        const css = utilities.interpolate(styles.innerHTML)(element.__state__);
-        const $template = document.createElement('template');
         const sentinel = document.createElement('style');
         
-        function getSlottedNodes(parentElement: HTMLUnknownElement) {
+        function getSlottedNodes(parentElement: HTMLUnknownElement | null) {
             if (!parentElement) return [];
             if (!parentElement.matches('as-frameless-slots')) return [];
             return Array.from(parentElement.childNodes);
@@ -56,19 +44,11 @@ export @customElement(TAGNAME, { extends: 'iframe' }) class FramelessElement ext
         
         function handleScriptLoad(e: Event) {
             if (e.target !== sentinel) return;
-            const { shadowRoot } = element as HTMLUnknownElement;
-            const shadow = shadowRoot!;
-            
-            $template.innerHTML = utilities.interpolate(innerHTML)(element.__state__);
             for (const child of slotted) element.appendChild(child);  // strongly-slotted content
-            shadow.appendChild(styles);
-            shadow.appendChild($template.content);
-            element.removeEventListener('load', handleScriptLoad, true);
-            module.remove();
             sentinel.remove();
+            element.removeEventListener('load', handleScriptLoad, true);
         }
         
-        styles.innerHTML = css;
         element.innerHTML = content;  // weakly-slotted content
         element.addEventListener('load', handleScriptLoad, true);
         element.appendChild(sentinel);

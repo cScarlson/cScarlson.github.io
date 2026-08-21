c101d23df7a86cddbb568e7cdd5ee1690cda0c2f
cScarlson <otocarlson@gmail.com>
2026-08-21 16:34:05 -0600

    -1: ENVIRONMENTS: HOTFIX...

--- magazinejs/app/core/data/registry.lcl.js
+++ magazinejs/app/core/data/registry.lcl.js
@@ -1,3 +1,3 @@
 
 export * from './registry.dev.js';
-export { default as localhost4000 } from 'http://localhost:4000/magazinejs/index.json' with { type: 'json' };
+// export { default as localhost4000 } from 'http://localhost:4000/magazinejs/index.json' with { type: 'json' };

65d27b82a05f2d90f4390e4de5421526a39a636f
Cody Skylark Carlson <cscarlson@Codys-MBP.attlocal.net>
2026-09-08 12:53:40 -0600

    -1: stabilizing mirror 0

--- env/environment.prd.js
+++ env/environment.prd.js
@@ -3,4 +3,5 @@ import { Environment } from './env.model.js';
 export const environment = new Environment({
     type: 'prd',
     origin: 'https://cscarlson.github.io',
+    worker: '/magazinejs/service.worker.prd.js',
 });
---
+++ magazinejs/service.worker.prd.js
@@ -0,0 +1,31 @@
+
+import { ServiceWorkerHandler } from './service.handler.env.js';
+
+const CLOUDFLARE_ORIGIN_LOCAL = 'http://localhost:4000';
+const worker = new (class ServiceWorkerLocal extends ServiceWorkerHandler {
+    
+    constructor(self) {
+        super();
+        self.addEventListener('activate', this, true);
+        self.addEventListener('fetch', this, true);
+    }
+    
+    ['http://localhost:4000/magazinejs/index.lcl.json'](request, e) {
+        const response = new Response('{}', { status: 200 });
+        e.respondWith(response);
+    }
+    
+    #handleFetch(e) {
+        this.handle(e);
+    }
+    
+    #handleActivation(e) {
+        e.waitUntil( self.clients.claim() );
+    }
+    
+    handleEvent(e) {
+        if (e.type === 'activate') return this.#handleActivation(e);
+        if (e.type === 'fetch') return this.#handleFetch(e);
+    }
+    
+})(self);

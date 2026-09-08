ce74e2d2c02abd063dc96a5cc2d1a4d952025744
Cody Skylark Carlson <cscarlson@Codys-MBP.attlocal.net>
2026-09-08 13:00:28 -0600

    -1: stabilizing mirror 0....

--- magazinejs/app/core/data/registry.lcl.js
+++ magazinejs/app/core/data/registry.lcl.js
@@ -1,3 +1,3 @@
 
-export { default as otocarlsoncloudflare } from 'http://localhost:4000/magazinejs/index.lcl.json' with { type: 'json' };
+// export { default as otocarlsoncloudflare } from 'http://localhost:4000/magazinejs/index.lcl.json' with { type: 'json' };
 export * from './registry.dev.js';
